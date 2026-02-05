72db404e9e25c342983899238e76ac3e05bb8704
oumar-julien gaudens <oumar-julien.gaudens@i-carre.net>
2026-02-05 14:05:33 +0100

    test1.1

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,15 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = function (eleventyConfig) {
-  /* =========================
-     COPIE DES FICHIERS STATIQUES
-  ========================== */
-  eleventyConfig.addPassthroughCopy("src/assets"); // CSS, JS, images
-
-  /* =========================
-     CONFIG GÉNÉRALE
-  ========================== */
+  // Copie TOUT le dossier assets (CSS, JS, images)
+  eleventyConfig.addPassthroughCopy("src/assets");
+  
   return {
-    pathPrefix: isProd ? "/oj-gaudens/" : "/", // ⚠️ NOM EXACT DU REPO
+    pathPrefix: isProd ? "/oj-gaudens/" : "/",
     dir: {
       input: "src",
       includes: "_includes",
       output: "docs"
-    },
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
-    templateFormats: ["md", "njk", "html"]
+    }
   };
 };
